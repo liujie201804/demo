f0beb3ed62ae68f0c8e64189c6aaf550709e26ee
jieli <liujie111>
2020-12-09 15:41:53 +0800

    集成xxl-job

---
+++ src/main/java/com/example/demo/mybatisPlus/task/TestHandler.java
@@ -0,0 +1,18 @@
+package com.example.demo.mybatisPlus.task;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jieliu
+ */
+@Component
+public class TestHandler {
+
+    @XxlJob(value = "demoJobHandler")
+    public ReturnT<String> execute() {
+        System.out.println("执行了TestHandler一次");
+        return ReturnT.SUCCESS;
+    }
+}
